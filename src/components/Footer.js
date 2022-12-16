14f06906019c7df6f1915a9ff207cfa4db34ea0c
Bitaw <stickynotes5@yahoo.com>
2022-12-16 07:46:45 -0600

    Styled Resume page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Footer() {
     return (
         <div>
-            <h1>Footer</h1>
+            <a id='GithubLink' href="https://github.com/patctan">GitHub</a>
+            <a id='LinkedInLInk' href="https://www.linkedin.com/in/patrick-tan-445ab4211/">LinkedIn</a>
+            <a id='StackOverflowLink' href="https://stackoverflow.com/users/19317673/patrick">Stack Overflow</a>
         </div>
     );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import "../styles/Resume.css"
 
 export default function Resume() {
     return (
         <div>
             <h1>Resume</h1>
+
+            <a id="resumeLink" href="https://docs.google.com/document/d/1MqdlPXHX-wcvQ4GIUJMmCmkEdn6qFoMinjTc_G4f51U/edit?usp=sharing" alt="resumeLink">Resume (Click Me!)</a>
+
+            <h2>Proficiencies:</h2>
+            <ul>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>html</li>
+                <li>React</li>
+                <li>MySQL</li>
+                <li>Node</li>
+                <li>Express</li>
+            </ul>
         </div>
     );
 }
