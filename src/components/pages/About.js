8b6a37f1d258d731fd7f4767ab243412f98314f3
Bitaw <stickynotes5@yahoo.com>
2022-12-16 00:51:20 -0600

    Added functions to About.js, Contact.js, Portfolio.js, and Resume.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                Hello! I'm a recent graduate of UT Austin's Coding Bootcamp. 
+                I am proficient in full-stack web development, and I'm looking 
+                forward to developing my skills further!
+            </p>
+        </div>
+    );
+}
